c2467576d6197cb2d64fb7ede6be59c1043363b7
iddqd2d <iddqd2d@gmail.com>
2019-05-20 14:55:38 +0300

    rlease

---
+++ src/main/java/com/spring/springmvc/config/ProjectServiceInterceptorAppConfig.java
@@ -0,0 +1,18 @@
+package com.spring.springmvc.config;
+
+import com.spring.springmvc.interceptors.LogInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Component
+public class ProjectServiceInterceptorAppConfig extends WebMvcConfigurerAdapter {
+    @Autowired
+    LogInterceptor logInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor);
+    }
+}
---
+++ src/main/java/com/spring/springmvc/controller/ProjectController.java
@@ -0,0 +1,45 @@
+package com.spring.springmvc.controller;
+
+import com.spring.springmvc.model.Project;
+import com.spring.springmvc.service.ProjectRepositoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+
+@Controller
+public class ProjectController {
+    @Autowired
+    private ProjectRepositoryService service;
+
+    @GetMapping("/home")
+    public ModelAndView projects() {
+        List<Project> projects = service.getAllProject();
+        return new ModelAndView("home", "projects", projects);
+    }
+
+    @PostMapping("/home")
+    public String addProject(Project project) {
+        service.createProject(project);
+        return "redirect:/home";
+    }
+
+    @RequestMapping("/add")
+    public ModelAndView createProject() {
+        return new ModelAndView("add-project", "project", new Project());
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProject(@PathVariable(name = "id") Integer id) {
+        service.deleteProjectById(id);
+        return "redirect:/home";
+    }
+
+    @GetMapping("/change/{id}")
+    public ModelAndView changeProject(@PathVariable(name = "id") Integer id) {
+        return new ModelAndView("update", "project", service.getProjectById(id));
+    }
+}
